2e7e1a3d28bcc27d6bb0b277e319da7a1bce5842
namchae.baik <namchae.baik@bankwareglobal.com>
2020-05-18 11:08:16 +0900

    각 서비스 별 db 분리

--- users-detail-service/src/main/java/spring/cloud/kubernetes/users/detail/service/UsersDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.cloud.kubernetes.users.detail.service;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersDetailRepository extends CrudRepository<UserDetail, String> {
-	public UserDetail findByUserId(String userId);
-}
